ac132b56e469eecfc4271ac7ed038a425713f565
Sasha <shb.sohrabi@gmail.com>
2024-11-25 22:20:33 +0100

    Add Layout Support, build the header of app, add summary section and period selection

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="flex justify-between items-center mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu v-model="selectedView" :options="TRANSACTION_VIEW_OPTIONS" />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { TRANSACTION_VIEW_OPTIONS } from '~/utils/constants';
+const selectedView = ref<string>(TRANSACTION_VIEW_OPTIONS[1]);
+</script>
