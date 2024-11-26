7444bebb73781a3d761067ed8d409c27bbeb8e8b
Sasha <shb.sohrabi@gmail.com>
2024-11-26 19:02:23 +0100

    Show percentage difference in Trending component

--- app/components/trend.vue
+++ app/components/trend.vue
@@ -10,12 +10,12 @@
       <USkeleton class="h-6 w-full" v-if="loading" />
       <div v-else class="flex space-x-1 items-center text-sm">
         <UIcon
-          name="i-heroicons-arrow-trending-up"
+          :name="icon"
           class="w-6 h-6"
-          :class="[color]"
+          :class="{ green: trendingUp, red: !trendingUp }"
         />
         <div class="text-gray-500 dark:text-gray-400">
-          30% vs last period
+          {{ percentageTrending }} vs last period
         </div>
       </div>
     </div>
@@ -23,13 +23,36 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   title: string;
   amount: number;
   lastAmount: number;
   color: string;
   loading: boolean;
 }>();
+
+const trendingUp = computed(() => props.amount >= props.lastAmount);
+
+const icon = computed(() =>
+  trendingUp.value
+    ? 'i-heroicons-arrow-trending-up'
+    : 'i-heroicons-arrow-trending-down'
+);
+
+const percentageTrending = computed(() => {
+  if (props.amount === 0 || props.lastAmount === 0) return '∞';
+
+  const bigger = Math.max(props.amount, props.lastAmount);
+  const lower = Math.min(props.amount, props.lastAmount);
+
+  const ratio = ((bigger - lower) / lower) * 100;
+
+  return `${Math.ceil(ratio)}%`;
+});
+
+onMounted(() => {
+  percentageTrending;
+});
 </script>
 
 <style scoped>
