175ecf90570a4b70e6ac5be376e4957118cacc38
Sasha <shb.sohrabi@gmail.com>
2024-11-26 23:42:10 +0100

    Add Supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,8 @@ export default defineNuxtConfig({
   },
 
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  supabase: {
+    redirect: false,
+  },
+});
